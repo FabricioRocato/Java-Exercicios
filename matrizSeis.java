a7b95f0bfcedc2645c92ca45ab6924d7d2b49df8
Fabricio Rocato <82914307+FabricioRocato@users.noreply.github.com>
2021-08-20 19:44:43 -0300

    Update and rename vetorSeis.java to matrizSeis.java

--- matrizSeis.java
+++ matrizSeis.java
@@ -2,8 +2,6 @@ package exercicios2;
 
 import java.util.Scanner;
 
-//FIZ O EXERCICIO DUAS VEZES, UMA VEZ COM NUMEROS DETERMINADOS
-//SEGUNDO COM OS VALORES SENDO COLOCADO PELO O USUARIO 
 public class vetorSeis {
 	public static void main(String[] args) {
 		int[][] matriz = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 },
